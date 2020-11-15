dd841e3a22e67ecc9e3384a91832c268875b131e
Daniyal Marofi <daniyaleight8@gmail.com>
2020-11-15 18:36:08 +0330

    moved main into a folder

---
+++ main/define.hpp
@@ -0,0 +1,9 @@
+#ifndef __DEFINE__
+#define __DEFINE__
+
+#define GAME_NAME "soccerSrars"
+#define GAME_WIDTH 800
+#define GAME_HEIGHT 480
+#define BACKGROUND_PATH "../Assets/field.jpg"
+
+#endif
---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "define.hpp"
+#include "../RSDL/rsdl.hpp"
+#include "../GameManager/GameManager.hpp"
+#include "../SoccerStars/SoccerStars.hpp"
+
+using namespace std;
+
+int main()
+{
+    SoccerStars game;
+    return 0;
+}
